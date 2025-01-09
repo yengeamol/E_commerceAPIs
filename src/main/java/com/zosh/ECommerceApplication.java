dca1e99a218558f74b5666f562ca6f55050b6aad
yengeamol <sonuyenge2002@gmail.com>
2025-01-08 15:59:18 -1200

    commit

---
+++ src/main/java/com/zosh/ECommerceApplication.java
@@ -0,0 +1,19 @@
+package com.zosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
